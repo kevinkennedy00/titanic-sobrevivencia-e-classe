e6834b592d0c3885d3ec053c863a50e565996362
kevinkennedy00 <kevinkennedy.dev@gmail.com>
2026-09-10 10:52:32 -0300

    fix: crop and enlarge toolbar artwork

--- app/web/src/App.tsx
+++ app/web/src/App.tsx
@@ -687,7 +687,7 @@ function App() {
             onClick={() => navigate(0)}
             aria-label="Voltar ao início"
           >
-            <span className="brand-mark brand-mark--image"><img src="/design-system/icone-toolbar.png" alt="" width="27" height="27" /></span>
+            <span className="brand-mark brand-mark--image"><img src="/design-system/icone-toolbar-crop.png" alt="" width="34" height="34" /></span>
             <span>TITANIC / 1912</span>
           </button>
           <div className="header-course">{data.course}</div>
